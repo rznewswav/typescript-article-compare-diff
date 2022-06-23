a6c5cf275d713ae7b4f83fa00cb796b277026624
raflie <raflie@newswav.com>
2022-06-23 12:23:56 +0800

    Refactor similarity calculation

--- src/word-diff.ts
+++ src/word-diff.ts
@@ -1,5 +1,6 @@
 import worddiff from 'word-diff';
 import { sample, sum } from "./dataset.util";
+import Papa from 'papaparse';
 
 const startNow = performance.now();
 function transformFeature(content: string) {
@@ -18,7 +19,7 @@ function diff(str1: string, str2: string) {
     return wdiff.map(d => {
         if ('remove' in d && 'add' in d) {
             const { remove = '', add = '' } = d
-            return (remove.trim().split(' ').length + add.trim().split(' ').length) / 2
+            return remove.trim().split(' ').length / str1.split(/\s/g).length + add.trim().split(' ').length / str2.split(/\s/g).length
         } else {
             return 0;
         }
@@ -28,18 +29,42 @@ function diff(str1: string, str2: string) {
 export const [testFile, testPool] = sample(transformFeature);
 
 function computeDiffFactor(string1: ReturnType<typeof transformFeature>[3], string2: ReturnType<typeof transformFeature>[3]) {
-    return Math.min(1, diff(string1, string2) / string1.split(/\s/g).length)
+    return diff(string1, string2) / 2
 }
 
 function computeDatasetSimilarity(source: typeof testFile, datasetPool: typeof testPool) {
-    return datasetPool.map(({fileName, content: [lang, title, url, content]}) => ({
-        diffFactor: computeDiffFactor(source.content[3], content),
-        fileName,
-        lang, title, url
-    }))
+    return [
+        {
+            contentDiffFactor: 0,
+            titleDiffFactor: 0,
+            fileName: source.fileName,
+            lang: source.content[0],
+            title: source.content[1],
+            url: source.content[2],
+            source: source.fileName,
+        },
+        ...datasetPool.map(({fileName, content: [lang, title, url, content]}) => ({
+            contentDiffFactor: computeDiffFactor(source.content[3], content),
+            titleDiffFactor: computeDiffFactor(source.content[1], title),
+            fileName,
+            lang, title, url,
+            source: source.fileName,
+        })),
+    ]
 }
-
-console.log(testFile.fileName, computeDatasetSimilarity(testFile, testPool).sort((a, b) => a.diffFactor - b.diffFactor))
+const computation = computeDatasetSimilarity(testFile, testPool).sort((a, b) => a.contentDiffFactor - b.contentDiffFactor)
 const endNow = performance.now()
+const timeTaken = endNow - startNow
+const columns = [
+    'timeTaken',
+    ...Object.keys(computation[0]),
+]
 
-console.log(`Time taken: ${endNow - startNow}ms`)
+Object.assign(computation[0], {
+    timeTaken
+})
+
+console.log(Papa.unparse(computation, {
+    columns
+}))
+
